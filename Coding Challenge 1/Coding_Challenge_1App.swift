969042c5e6ac90d5a7a2280d9423675d3fa148c8
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-23 16:31:09 +0100

    Initial Commit

---
+++ Coding Challenge 1/Coding_Challenge_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Coding_Challenge_1App.swift
+//  Coding Challenge 1
+//
+//  Created by Jan Armbrust on 23.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Coding_Challenge_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
