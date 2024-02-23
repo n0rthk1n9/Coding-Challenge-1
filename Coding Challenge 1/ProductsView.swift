07d8c58cef73f62b5609a4435c9bfe7642318cb9
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-23 18:11:35 +0100

    add badge to see if product is available

--- Coding Challenge 1/ProductsView.swift
+++ Coding Challenge 1/ProductsView.swift
@@ -12,8 +12,8 @@ struct ProductsView: View {
 
   private var gridItemWidth: CGFloat {
     let screenWidth = UIScreen.main.bounds.width
-    let totalSpacing = 10.0 * (CGFloat(columnsCount) - 1) // Total spacing between columns
-    let totalPadding = 10.0 * 2 // Left and right padding
+    let totalSpacing = 20.0 * (CGFloat(columnsCount) - 1)
+    let totalPadding = 20.0 * 2
     let availableWidth = screenWidth - totalSpacing - totalPadding
     return availableWidth / CGFloat(columnsCount)
   }
@@ -24,36 +24,52 @@ struct ProductsView: View {
   }
 
   var body: some View {
-    ScrollView {
-      LazyVGrid(columns: columns, spacing: 10) {
-        ForEach(viewModel.products) { product in
-          VStack(alignment: .leading, spacing: 0) {
-            AsyncImage(url: product.imageUrl) { image in
-              image
-                .resizable()
-            } placeholder: {
-              ProgressView()
-            }
-            .aspectRatio(contentMode: .fit)
-            .frame(height: 200)
-            .clipped()
-            .cornerRadius(8)
-
-            Text(product.title)
-              .font(.headline)
-              .multilineTextAlignment(.leading)
-              .lineLimit(2)
-              .padding(.top, 5)
+    NavigationStack {
+      ScrollView {
+        LazyVGrid(columns: columns, spacing: 20) {
+          ForEach(viewModel.products) { product in
+            VStack(alignment: .leading) {
+              AsyncImage(url: product.imageUrl) { image in
+                image
+                  .resizable()
+              } placeholder: {
+                ProgressView()
+              }
+              .scaledToFill()
+              .frame(maxWidth: .infinity, maxHeight: 200, alignment: .center)
               .clipped()
+              .cornerRadius(8)
+              .overlay(
+                Text(product.isAvailable ? "Auf Lager" : "Ausverkauft")
+                  .fontWeight(Font.Weight.medium)
+                  .font(Font.system(size: 12))
+                  .foregroundColor(Color.white)
+                  .padding([.leading, .trailing], 8)
+                  .padding([.top, .bottom], 4)
+                  .background(product.isAvailable ? Color.green.opacity(0.8) : Color.red.opacity(0.8))
+                  .clipShape(
+                    .rect(
+                      topLeadingRadius: 8,
+                      bottomLeadingRadius: 0,
+                      bottomTrailingRadius: 8,
+                      topTrailingRadius: 0
+                    )
+                  ),
+                alignment: .topLeading
+              )
+
+              Text(product.title)
+                .font(.headline)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .padding(.top, 5)
+                .clipped()
+            }
           }
-          .padding()
-          .overlay(
-            RoundedRectangle(cornerRadius: 8)
-              .stroke(Color.primary, lineWidth: 1)
-          )
         }
+        .padding(.horizontal)
       }
-      .padding(.horizontal)
+      .navigationTitle("Produktübersicht")
     }
     .onAppear {
       viewModel.getProducts()
