42440b2002afbe3a2cc240e22b9d06f97f0f6f5b
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-24 14:07:45 +0100

    add fixture for variant type

--- Coding Challenge 1/Models/Variant.swift
+++ Coding Challenge 1/Models/Variant.swift
@@ -12,4 +12,27 @@ struct Variant: Codable, Equatable, Hashable, Identifiable {
   let color: String
   let imageUrl: URL
   let isAvailable: Bool
+
+  static func fixture(
+    id: Int = 0,
+    color: String = "",
+    imageUrl: URL = URL(string: "")!,
+    isAvailable: Bool = true
+  ) -> Variant {
+    Variant(
+      id: id,
+      color: color,
+      imageUrl: imageUrl,
+      isAvailable: isAvailable
+    )
+  }
+
+  static var allProperties: Variant {
+    .fixture(
+      id: 4713,
+      color: "kastanienbraun",
+      imageUrl: URL(string: "https://image01.bonprix.de/assets/275x385/1600856915/20167389-2HSkctsd.jpg")!,
+      isAvailable: true
+    )
+  }
 }
